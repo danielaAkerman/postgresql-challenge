1bd63e3abff3300437b87e1d4a7278ed54165427
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-01-27 16:45:50 -0300

    CRUD

--- index.ts
+++ index.ts
@@ -8,20 +8,37 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.get("/product", (req, res) => {
-  Product.findAll().then(snap=>{
-    res.json(snap.map((i) => i.dataValues))
-  })})
-  
-app.get("/product/:title", (req, res) => {
-  const { title } = req.params;
-  Product.findAll({
-    where:{title}
-  }).then(snap=>{
-    res.json(snap.map((i) => i.dataValues))
-  })})
+app.get("/products", async (req, res) => {
+  const allProducts = await Product.findAll();
+  res.json(allProducts);
+  // res.json(allProducts.map((i)=>i.dataValues))
+});
 
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const productFound = await Product.findAll({
+    where: { id },
+  });
+  res.json(productFound);
+});
 
+app.post("/products", async (req, res) => {
+  const newProduct = await Product.create(req.body);
+  res.json(newProduct);
+});
+
+app.patch("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const modifiedProduct = await Product.update({ title }, { where: { id } });
+  res.json(modifiedProduct);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.destroy({ where: { id } });
+  res.json({ message: "Deleted product" });
+});
 
 app.listen(port, () => {
   console.log("Corriendo en puerto http://localhost:" + port);
