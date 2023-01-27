911e91ed4f484c00f1c73f6e26fd551a3a10f8b6
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-01-27 15:53:17 -0300

    Construyendo API

--- index.ts
+++ index.ts
@@ -8,64 +8,21 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-// LAS BASES DE DATOS RELACIONALES APORTAN ESTRUCTURA Y COMPLEJIDAD
-// OFRECEN OPORTUNIDADES PARA ESCALAR
-// COLLECTIONS Y DOCS EN NOSQL
-// SON TABLAS Y REGISTROS EN SQL
+app.get("/product", (req, res) => {
+  Product.findAll().then(snap=>{
+    res.json(snap.map((i) => i.dataValues))
+  })})
+  
+app.get("/product/:title", (req, res) => {
+  const { title } = req.params;
+  Product.findAll({
+    where:{title}
+  }).then(snap=>{
+    res.json(snap.map((i) => i.dataValues))
+  })})
 
-// USAMOS UNA DB PostGres PERO ALOJADA EN LA NUBE. (as a Service)
 
-// Las librerías ORM nos ayudan a interactuar con las SQL sin conocer el lenguaje
-// ORM: Object Relational Mapping (mapeo objeto-relacional)
-// Parsean los valores q recibimos desde DB y lo utilizamos como un objeto
-// Tamb parsea nuestros objetos y crea las queries necesarias para interactuar con la DB
-// Luego de crear o modificar tablas, se lo tenemos que indicar a Sequalize.
-// La opción alter es la que vamos a usar de ahora en más
-// para modificar tablas sin empezar de cero,
-// a diferencia de la opción force que elimina todo y lo vuelve a crear.
-// La opción alter le indica a Sequalize que modifique lo menos posible.
 
-async function main() {
-  // const resp = await sequelize.sync({ alter: true });
-  // console.log(resp)
-
-  // const productNew = await Product.create({
-  //   title: "Parlante",
-  //   price: 2800,
-  // });
-
-  const todos = await Product.findAll();
-  console.log({"PRODUCTOS:": todos.map((i) => i.dataValues)});
-
-  // console.log("EL PRODUCTO:", productNew);
-
-  // const todos = await User.findAll();
-  // console.log({"REGISTROS": todos.map((i) => i.dataValues.otrocampo)});
-
-  // const danie = await User.findByPk(2);
-  // console.log(danie.dataValues.username)
-
-  // const dani2 = await User.findAll({
-  //   where: {
-  //     otrocampo: "Hola",
-  //   },
-  // });
-
-  // console.log(dani2.map((i) => i.dataValues));
-}
-main();
-
-// const usersCreados = await User.findAll();
-
-// for (const u of usersCreados) {
-//   console.log(u.dataValues);
-// }
-
-//   for (const u of usersCreados) {
-//     console.log(u.toJSON());
-//   }
-
-// console.log({ "Users Creados": usersCreados.map((i) => i.dataValues) });
 app.listen(port, () => {
   console.log("Corriendo en puerto http://localhost:" + port);
 });
